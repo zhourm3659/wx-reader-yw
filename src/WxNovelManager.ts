52f55053cd49781bba18a7fcc23fb47148cc8a90
p_rminzhou <p_rminzhou@tencent.com>
2024-08-07 22:06:18 +0800

    feat: lint + prettier

--- src/WxNovelManager.ts
+++ src/WxNovelManager.ts
@@ -1,3 +1,4 @@
+import chalk from 'chalk';
 interface NovelPlugin {
   onPageLoad(callback: (data: {id: string}) => void): void;
   getNovelManager(id: string): any;
@@ -5,21 +6,22 @@ interface NovelPlugin {
 }
 
 export default class WxNovelManager {
-  private wxNovelManager: any = null;
+  public static init(novelPlugin: NovelPlugin, cb?: (manager: any) => void): WxNovelManager {
+    return new WxNovelManager(novelPlugin, cb);
+  }
+
+  private wxNovelManager: WxNovelManager | null = null;
 
   // 防止外部直接实例化
   private constructor(novelPlugin: NovelPlugin, cb?: (manager: any) => void) {
-    console.log('enter......');
-    if (!novelPlugin) throw new Error('novelPlugin对象不能为空');
+    console.log(chalk.green('init WxNovelManager'));
+    if (!novelPlugin) throw new Error('novelPlugin is required');
+
     // 此处是监听 novel-plugin 页面的加载
-    novelPlugin.onPageLoad(({id}) => {
+    novelPlugin.onPageLoad(({id}: {id: string}) => {
       // 此处赋值，后续使用
       this.wxNovelManager = novelPlugin.getNovelManager(id);
-      cb!(this.wxNovelManager);
+      cb && cb(this.wxNovelManager);
     });
-  }
-
-  public static init(novelPlugin: NovelPlugin, cb?: (manager: any) => void): WxNovelManager {
-    return new WxNovelManager(novelPlugin, cb);
   }
 }
